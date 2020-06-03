4edd982093a9b35fe808254f1073c4916caf5078
Jeffrey <Jeffrey@simpleyapp.com>
2020-06-03 13:09:21 -0400

    Initial Commit

---
+++ Choose Your Side/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Choose Your Side
+//
+//  Created by Jeffrey  on 6/3/20.
+//  Copyright © 2020 BMCC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
